b84d4acb56837629436ddd8e8fa0678eee6c5c64
Loganathan M <Loganathan_M3@Dellteam.com>
2021-06-21 09:35:30 +0530

    Adding .Net5 console App

---
+++ StringManipulation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace StringManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringConversion();
+        }
+
+        private static void StringConversion()
+        {
+            string testString = "tHiS SHoULd bE iN LOwEr cAse";
+            TextInfo currentTextInfo = CultureInfo.CurrentCulture.TextInfo;
+
+            string result = testString.ToLower();
+            Console.WriteLine(result);
+
+            result = testString.ToUpper();
+            Console.WriteLine(result);
+
+            result = currentTextInfo.ToTitleCase(testString);
+            Console.WriteLine(result);
+
+        }
+    }
+}
